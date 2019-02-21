5f14c1675064a24eb2035bdbb66360af8a14ad9f
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-21 21:19:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
       "Transaction rejected. Please check your account balance."
     end
   end
-
+  
   def reverse_transfer
     if self.status == "complete"
       sender.balance += amount
